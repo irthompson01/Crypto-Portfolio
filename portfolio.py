99d8df4660802a97565729933d9b6b77b1048a66
irthompson01 <36970433+irthompson01@users.noreply.github.com>
2022-01-14 18:59:32 -0500

    Add files via upload

--- portfolio.py
+++ portfolio.py
@@ -27,12 +27,11 @@ def load_transaction_data():
     #del df['Unnamed: 0']
     return df
 
-@st.cache(persist=True)
 def load_CMC_data():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
     parameters = {
-        "symbol": 'BTC,ETH,ADA,XMR',
+        "symbol": 'BTC,ETH,ADA,XMR,ERG,LINK',
         "convert": "USD"
     }
 
@@ -56,7 +55,7 @@ def main():
     st.sidebar.title("Select a Portfolio:")
     portfolio = st.sidebar.selectbox(
      '',
-     ('Home','Ross & Amy', 'Casey y Luca', 'Ian'))
+     ('Home','Ross & Amy', 'Casey y Luca'))
 
     if portfolio == 'Home':
          priceData()
@@ -69,26 +68,46 @@ def main():
 
 def priceData():
     st.title("Current Crypto Prices")
+
     col1, col2, col3 = st.columns(3)
     col1.metric("Bitcoin", "$"+str(round(prices['data']['BTC']['quote']['USD']['price'], 2)), str(round(prices['data']['BTC']['quote']['USD']['percent_change_24h'], 2))+"%")
+    with col1.expander("BTC Metrics"):
+        getMetrics("BTC")
+    col1.metric("Monero", "$"+str(round(prices['data']['XMR']['quote']['USD']['price'], 2)), str(round(prices['data']['XMR']['quote']['USD']['percent_change_24h'], 2))+"%")
+    with col1.expander("XMR Metrics"):
+        getMetrics("XMR")
+
     col2.metric("Ethereum", "$"+str(round(prices['data']['ETH']['quote']['USD']['price'], 2)), str(round(prices['data']['ETH']['quote']['USD']['percent_change_24h'], 2))+"%")
+    with col2.expander("ETH Metrics"):
+        getMetrics("ETH")
+    col2.metric("Ergo", "$"+str(round(prices['data']['ERG']['quote']['USD']['price'], 2)), str(round(prices['data']['ERG']['quote']['USD']['percent_change_24h'], 2))+"%")
+    with col2.expander("ERG Metrics"):
+        getMetrics("ERG")
+
     col3.metric("Cardano", "$"+str(round(prices['data']['ADA']['quote']['USD']['price'], 2)), str(round(prices['data']['ADA']['quote']['USD']['percent_change_24h'], 2))+"%")
+    with col3.expander("ADA Metrics"):
+        getMetrics("ADA")
+    col3.metric("Chainlink", "$"+str(round(prices['data']['LINK']['quote']['USD']['price'], 2)), str(round(prices['data']['LINK']['quote']['USD']['percent_change_24h'], 2))+"%")
+    with col3.expander("LINK Metrics"):
+        getMetrics("LINK")
 
 def showData(owner):
     port_dict = {"RA": "Ross & Amy", "CL": "Casey y Luca"}
     st.title(port_dict[owner] + "'s Crypto Portfolio")
     sub = df[df['Owner'] == owner]
 
+    ##### Total Invested/Current Value #####
+    st.header("Investment: $" + str(2500))
 
 
+    ##### Breakdown by Coin #####
     cryptos = sub["Symbol"].unique()
-
     for tick in cryptos:
         col1, col2 = st.columns(2)
         sub_tick = sub[sub['Symbol'] == tick]
         total = getTotalAmount(sub_tick)
         col1.metric(tick, total)
-        col2.metric("Value (USD)", total*round(prices['data'][tick]['quote']['USD']['price'], 2))
+        col2.metric("Value (USD)", round(total*prices['data'][tick]['quote']['USD']['price'], 2))
 
 
     #st.dataframe(sum_amount)
@@ -101,6 +120,11 @@ def getTotalAmount(df):
     for i in sum_amount:
         total += float(str(i).replace(',',''))
     return total
+
+def getMetrics(tick):
+    for i in prices['data'][tick]['quote']['USD']:
+        if i != 'last_updated':
+            st.write(i.replace("_", " ").capitalize() + ": " + str(round(prices['data'][tick]['quote']['USD'][i], 2)))
 
 if __name__ == "__main__":
     main()
